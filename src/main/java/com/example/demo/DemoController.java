bca2af77751ec2d2ff348e365a047d7827e19db5
sanjay <sanjaynagarajanramnad@gmail.com>
2026-07-07 13:52:13 +0530

    URL access

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,12 +1,22 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/v1")
 public class DemoController {
     @GetMapping("/hello")
     String Hello(){
         return "Hello World";
     }
+    @GetMapping("/name/{name}")
+    String name(@PathVariable String name){
+        return "Hello "+name;
+    }
+
+    @GetMapping("/Req")
+    String nameReq(@RequestParam String id){
+        return id;
+    }
+
 }
